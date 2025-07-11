bfe761b5ceb985b9e03325a5bb26b01bc84bab20
simon <simonodoh3@gmail.com>
2025-07-11 04:15:27 -0700

    Add script to handle number of arguments

---
+++ 2-arguments.js
@@ -0,0 +1,4 @@
+const args = ProcessingInstruction.argv.slice(2);
+if (args.length ==0) { console.log("No argument"); }
+else if (args.lenght == 1) { console.log("Argument found"); }
+else { console.log("Arguments found"); })
