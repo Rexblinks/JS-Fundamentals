2b61aee4bc7d92c52737e0cd53a97df5d93bcba2
simon <simonodoh3@gmail.com>
2025-07-11 11:15:58 -0700

    Fix: correct logic and syntax in 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-const arg = ProcessingInstruction.argv[2];
+const arg = process.argv[2];
+const number = parseInt(arg);
 if (!isNaN(number)) { console.log(`My number: ${number}`);
 } else { console.log('Not a number'); }}
